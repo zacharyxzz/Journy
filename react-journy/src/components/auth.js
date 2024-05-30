9440181eb318f9d986fe69d677b7ddf5c4617846
xiebawang <xiebawang@hotmail.com>
2024-05-30 22:00:16 +0800

    Added Login Signup

--- react-journy/src/components/auth.js
+++ react-journy/src/components/auth.js
@@ -1,27 +1,49 @@
 import { useState } from "react";
-import { auth } from '../config/firebase';
+import { auth } from "../config/firebase";
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import "./auth.css";
 
 export const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(""); 
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const signIn = async () => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+        switch (error.code) {
+            case 'auth/weak-password':
+              setErrorMessage('Please choose a password with more than 6 charaters.');
+              break;
+            case 'auth/email-already-in-use':
+              setErrorMessage('The email address is already registered. Please use a different email.');
+              break;
+            default:
+              setErrorMessage(error.message); 
+        }
+    }
+};
 
-    const signIn = async () => {
-        await createUserWithEmailAndPassword(auth, email, password);
-    };
-
-    return (
-        <div>
-            <input 
-                placeholder = "Email..."
-                onChange={(e) => setEmail(e.target.value)}
-            />
-            <input 
-                placeholder = "Password..."
-                type="password"
-                onChange={(e) => setPassword(e.target.value)}/>
-            <button onClick={signIn}>Sign in</button>
-        </div>
-    )
-}
+  return (
+    <div className="wrapper">
+      <div className="box">
+        <input 
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input 
+          placeholder="Password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {errorMessage && (
+          <div className="error">
+            {errorMessage}
+          </div>
+        )}
+        <button onClick={signIn}>Sign Up</button>
+      </div>
+    </div>
+  );
+};
