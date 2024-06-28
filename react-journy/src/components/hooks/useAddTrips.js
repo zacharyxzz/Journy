f06ea2c1455a6a98b98ed0fadc411c65e70feffd
zac <rainfell@berkeley.edu>
2024-06-28 12:50:13 -0700

    Changed firestore structure

--- react-journy/src/components/hooks/useAddTrips.js
+++ react-journy/src/components/hooks/useAddTrips.js
@@ -1,9 +1,11 @@
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { db } from "../../config/firebase";
+import { doc } from "firebase/firestore";
 import { useGetUserInfo } from "./useGetUserInfo";
 export const useAddTrips = () => {
-  const tripsCollectionRef = collection(db, "trips");
   const { email } = useGetUserInfo();
+  const userDocRef = doc(db, "users", email);
+  const tripsCollectionRef = collection(userDocRef, "trips");
   const addTrips = async ({ location, description, startDate, endDate }) => {
     try {
       const formattedStartDate = startDate.toISOString().split("T")[0];
@@ -14,7 +16,7 @@ export const useAddTrips = () => {
         startDate: formattedStartDate,
         endDate: formattedEndDate,
         createdAt: serverTimestamp(),
-        email,
+        createdBy: email
       });
     } catch (error) {
       console.error("Error adding trip: ", error);
